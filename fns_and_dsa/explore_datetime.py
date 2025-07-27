1612eefea9a2615f67b24013c5117570e07bad5d
themeykar <chukwuemekaorji1759@gmail.com>
2025-07-27 20:16:12 +0100

    exploring datetime module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+from datetime import datetime, date, timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print(f"Current date and time: {current_date}")
+
+display_current_datetime()
+
+def calculate_future_date():
+    today = date.today()
+    days_number = int(input("Enter number of days: "))
+    future_date = today + timedelta(days_number)
+    print(f"Future date: {future_date}")
+
+calculate_future_date()
