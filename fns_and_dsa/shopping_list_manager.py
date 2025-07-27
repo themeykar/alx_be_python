1afa98a2ed485bdf90aa5265aef848f53067378c
themeykar <chukwuemekaorji1759@gmail.com>
2025-07-27 21:01:20 +0100

    shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,7 +10,7 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = int(input("Enter your choice: "))
         if choice == "1":
             shopping_list.append()
         elif choice == "2":
