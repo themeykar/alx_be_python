fd4af5f65a1c869ce4476b0bc618f29f2ebe55b1
themeykar <chukwuemekaorji1759@gmail.com>
2025-08-03 19:54:56 +0100

    writing unit tests for simple calculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,58 +6,46 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add_positive_number(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(5, 3), 8)
-
-    def test_add_negative_number(self):
         self.assertEqual(self.calc.add(-5, -3), -8)
-
-    def test_add_positive_and_negative(self):
         self.assertEqual(self.calc.add(10, -4), 6)
 
-    def test_subtract_positive_numbers(self):
+
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 2), 3)
-
-    def test_subtract_negative_numbers(self):
         self.assertEqual(self.calc.subtract(-5, -2), -3)
-
-
-    def test_subtract_with_zero(self):
         self.assertEqual(self.calc.subtract(7,0), 7)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
 
-    def test_multiply_positive_numbers(self):
-        self.assertEqual(self.calc.multiply(7, 7), 49)
 
-    def test_multiply_negative_numbers(self):
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(7, 7), 49)
         self.assertEqual(self.calc.multiply(-4, -3), 12)
 
 
-    def test_divide_positive_number(self):
-        self.assertEqual(self.calc.divide(25, 5), 5)
 
-    def test_divide_by_zero(self):
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(25, 5), 5)
         result = self.calc.divide(10,0)
         self.assertIsNone(result)
         result = self.calc.divide(-10,0)
         self.assertIsNone(result)
         result = self.calc.divide(0,0)
         self.assertIsNone(result)
-
-    def test_divide_zero_by_number(self):
         result = self.calc.divide(0, 5)
         self.assertEqual(result, 0)
         result = self.calc.divide(0, -3)
         self.assertEqual(result, 0)
-
-    def test_divide_negative_numbers(self):
         result = self.calc.divide(-12, -3)
         self.assertEqual(result, 4)
         result = self.calc.divide(-15, 3)
         self.assertEqual(result, -5)
         result = self.calc.divide(20, -4)
         self.assertEqual(result, -5)
+
 
 
 if __name__ == "__main__":
